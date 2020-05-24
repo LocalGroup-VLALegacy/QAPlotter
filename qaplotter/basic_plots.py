fceaca1ff649c42fd9594cd8e9a65c2554a11861
e-koch <koch.eric.w@gmail.com>
2020-05-24 12:14:21 -0700

    Add displaying ant1 and ant2 on hover

--- qaplotter/basic_plots.py
+++ qaplotter/basic_plots.py
@@ -1,11 +1,20 @@
 
 import plotly.graph_objects as go
+import numpy as np
 
 
-def basic_scatter(tab, xvar='x', yvar='y'):
+def basic_scatter(tab, xvar='x', yvar='y', hover='ants'):
+
+    if hover == 'ants':
+        hovertemplate = 'Ant1: %{customdata[0]} <br>Ant2 %{customdata[1]}'
+
+        custom_data = np.vstack((tab['ant1'].tolist(),
+                                 tab['ant2'].tolist())).T
 
     fig = go.Figure(data=go.Scattergl(x=tab[xvar],
                                       y=tab[yvar],
                                       mode='markers',
-                                      marker=dict(color='black')))
+                                      marker=dict(color='black'),
+                                      customdata=custom_data,
+                                      hovertemplate=hovertemplate))
     fig.show()
