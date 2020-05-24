acf6d50ab43830dd13a5c85c88f0a53f386be52e
e-koch <koch.eric.w@gmail.com>
2020-05-24 12:35:45 -0700

    Pass meta data into labels, title, etc

--- qaplotter/basic_plots.py
+++ qaplotter/basic_plots.py
@@ -1,9 +1,10 @@
 
 import plotly.graph_objects as go
+import plotly.express as px
 import numpy as np
 
-
-def basic_scatter(tab, xvar='x', yvar='y', hover='ants'):
+def basic_scatter(tab, xvar='x', yvar='y', hover='ants',
+                  colorvar='ant1', meta=None):
 
     if hover == 'ants':
         # Note that the slicing here is OPPOSITE the numpy convention.
@@ -18,7 +19,23 @@ def basic_scatter(tab, xvar='x', yvar='y', hover='ants'):
     fig = go.Figure(data=go.Scattergl(x=tab[xvar],
                                       y=tab[yvar],
                                       mode='markers',
-                                      marker=dict(color='black'),
+                                      marker=dict(size=16,
+                                                  cmax=tab[colorvar].max(),
+                                                  cmin=tab[colorvar].min(),
+                                                  color=tab[colorvar].tolist(),
+                                                  colorscale=px.colors.qualitative.Safe),
                                       customdata=custom_data,
                                       hovertemplate=hovertemplate))
+    if meta is not None:
+        fig.update_layout(
+            title=f"Field: {meta['field']}<br>SPW: {meta['spw']}<br>Scan: {meta['scan']}<br>MS: {meta['vis']}",
+            # NEED TO PASS x, y names!!
+            xaxis_title="uv-distance (m)",
+            yaxis_title="Amplitude",
+            font=dict(
+                family="Courier New, monospace",
+                size=15,
+                color="#7f7f7f")
+        )
+
     fig.show()
