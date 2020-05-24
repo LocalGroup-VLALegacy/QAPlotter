53e9eb8b5365f23d25fc18969f5b70b611feaa59
e-koch <koch.eric.w@gmail.com>
2020-05-24 12:17:26 -0700

    Note for the slicing convention for hovertemplate

--- qaplotter/basic_plots.py
+++ qaplotter/basic_plots.py
@@ -6,10 +6,14 @@ import numpy as np
 def basic_scatter(tab, xvar='x', yvar='y', hover='ants'):
 
     if hover == 'ants':
+        # Note that the slicing here is OPPOSITE the numpy convention.
+        # Hence the transpose on custom_data.
         hovertemplate = 'Ant1: %{customdata[0]} <br>Ant2 %{customdata[1]}'
 
         custom_data = np.vstack((tab['ant1'].tolist(),
                                  tab['ant2'].tolist())).T
+    else:
+        raise NotImplementedError("")
 
     fig = go.Figure(data=go.Scattergl(x=tab[xvar],
                                       y=tab[yvar],
