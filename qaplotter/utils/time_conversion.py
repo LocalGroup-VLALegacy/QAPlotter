28d66f61d10d7024b2bfc032fccda5190dc9fa18
e-koch <koch.eric.w@gmail.com>
2020-06-27 15:02:00 -0600

    Return time strings in CASA format

--- qaplotter/utils/time_conversion.py
+++ qaplotter/utils/time_conversion.py
@@ -4,7 +4,7 @@ from astropy.time import Time
 import astropy.units as u
 
 
-def vla_time_conversion(time_mjd):
+def vla_time_conversion(time_mjd, return_casa_string=True):
     '''
     Convert MJD seconds outputted in CASA txt files into a human-readable
     format for flagging purposes.
@@ -17,4 +17,15 @@ def vla_time_conversion(time_mjd):
 
     time = Time(time_mjd * u.second, format='mjd', location=vla_loc)
 
+    if return_casa_string:
+        # Replace spaces and - with / to match CASA
+        # If one element, returns a str
+        if isinstance(time.iso, str):
+            return time.iso.replace(" ", "/").replace("-", "/")
+        # Else returns an array of strings
+        else:
+            return [time_str.replace(" ", "/").replace("-", "/")
+                    for time_str in time.iso]
+
+    # Otherwise return the Time object
     return time
