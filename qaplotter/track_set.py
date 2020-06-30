a5e75ced65e1d12899d477565a3ae9158b563904
e-koch <koch.eric.w@gmail.com>
2020-06-29 21:04:24 -0600

    General function to save BP figures

--- qaplotter/track_set.py
+++ qaplotter/track_set.py
@@ -2,8 +2,9 @@
 from glob import glob
 import os
 
-from .utils import read_field_data_tables
-from .basic_plots import target_scan_figure, calibrator_scan_figure
+from .utils import read_field_data_tables, read_bpcal_data_tables
+from .field_plots import target_scan_figure, calibrator_scan_figure
+from .bp_plots import bp_amp_phase_figures
 
 
 def make_field_plots(folder, output_folder):
@@ -35,7 +36,7 @@ def make_field_plots(folder, output_folder):
 
             fig = target_scan_figure(table_dict, meta_dict, show=False)
 
-        elif len(table_dict.keys()) == 7:
+        elif len(table_dict.keys()) == 8:
 
             fig = calibrator_scan_figure(table_dict, meta_dict, show=False)
 
@@ -43,4 +44,21 @@ def make_field_plots(folder, output_folder):
             raise ValueError(f"Found {len(table_dict.keys())} tables for {field} instead of 3 or 7.")
 
         out_html_name = f"{field}_plotly_interactive.html"
+        fig.write_html(f"{output_folder}/{out_html_name}")
+
+
+def make_BP_plots(folder, output_folder):
+
+    table_dict, meta_dict = read_bpcal_data_tables(folder)
+
+    figs = bp_amp_phase_figures(table_dict, meta_dict,
+                                nspw_per_figure=4)
+
+    # Make output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+
+    for i, fig in enumerate(figs):
+
+        out_html_name = f"BP_amp_phase_plotly_interactive_{i}.html"
         fig.write_html(f"{output_folder}/{out_html_name}")
