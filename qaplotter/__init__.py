8ee02ea4904d5adc5624ed1317fea251eac1366c
e-koch <koch.eric.w@gmail.com>
2020-07-15 14:19:46 -0600

    Make top-level importable functions

--- qaplotter/__init__.py
+++ qaplotter/__init__.py
@@ -6,4 +6,6 @@
 from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
 
-__all__ = []
+from .track_set import make_all_plots, make_field_plots, make_BP_plots
+
+__all__ = ['make_all_plots', 'make_field_plots', 'make_BP_plots']
