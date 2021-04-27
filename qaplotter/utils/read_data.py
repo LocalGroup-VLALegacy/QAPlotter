b88e5f21a0107315ebe4d2d56ca3a103260bdce5
e-koch <koch.eric.w@gmail.com>
2021-04-27 15:10:48 -0400

    Add read functions for all cal table txt files

--- qaplotter/utils/read_data.py
+++ qaplotter/utils/read_data.py
@@ -118,8 +118,8 @@ def read_bpcal_data_tables(inp_path):
         table_dict[tab_type] = {}
         meta_dict[tab_type] = {}
 
-    amp_tab_names = glob(f"{inp_path}/*finalBPcal_amp*.txt")
-    phase_tab_names = glob(f"{inp_path}/*finalBPcal_phase*.txt")
+    amp_tab_names = glob(f"{inp_path}/*finalBPcal_freq_amp*.txt")
+    phase_tab_names = glob(f"{inp_path}/*finalBPcal_freq_phase*.txt")
 
     if len(amp_tab_names) != len(phase_tab_names):
         raise ValueError("Number of BP amp tables does not match BP phase tables.: "
@@ -150,5 +150,227 @@ def read_bpcal_data_tables(inp_path):
 
         meta_dict['amp'][spw] = amp_out[1]
         meta_dict['phase'][spw] = phase_out[1]
+
+    return table_dict, meta_dict
+
+
+def read_delay_data_tables(inp_path):
+    '''
+    Read in the BP txt files for amp and phase.
+    '''
+
+    table_dict = dict()
+    meta_dict = dict()
+
+    # Table types:
+    tab_types = ["delay"]
+
+    for tab_type in tab_types:
+        table_dict[tab_type] = {}
+        meta_dict[tab_type] = {}
+
+    delay_tab_names = glob(f"{inp_path}/*finaldelay_freq_delay*.txt")
+
+    # Sort by SPW and create a text
+    ant_nums = [int(tab.rstrip(".txt").split("ant")[1]) for tab in delay_tab_names]
+
+    for ant in ant_nums:
+
+        # There aren't many to loop through.
+        for ant_name in delay_tab_names:
+
+            if f"_ant{ant}.txt" in ant_name:
+                break
+
+        delay_out = read_casa_txt(ant_name)
+
+        table_dict['delay'][ant] = delay_out[0]
+
+        meta_dict['delay'][ant] = delay_out[1]
+
+    return table_dict, meta_dict
+
+
+def read_BPinitialgain_data_tables(inp_path):
+    '''
+    Read in the BP txt files for amp and phase.
+    '''
+
+    table_dict = dict()
+    meta_dict = dict()
+
+    # Table types:
+    tab_types = ["phase"]
+
+    for tab_type in tab_types:
+        table_dict[tab_type] = {}
+        meta_dict[tab_type] = {}
+
+    bpinitial_tab_names = glob(f"{inp_path}/*finalBPinitialgain_time_phase*.txt")
+
+    # Sort by SPW and create a text
+    ant_nums = [int(tab.rstrip(".txt").split("ant")[1]) for tab in bpinitial_tab_names]
+
+    for ant in ant_nums:
+
+        # There aren't many to loop through.
+        for ant_name in bpinitial_tab_names:
+
+            if f"_ant{ant}.txt" in ant_name:
+                break
+
+        phase_out = read_casa_txt(ant_name)
+
+        table_dict['phase'][ant] = phase_out[0]
+
+        meta_dict['phase'][ant] = phase_out[1]
+
+    return table_dict, meta_dict
+
+
+def read_phaseshortgaincal_data_tables(inp_path):
+    '''
+    Read in the BP txt files for amp and phase.
+    '''
+
+    table_dict = dict()
+    meta_dict = dict()
+
+    # Table types:
+    tab_types = ["phase"]
+
+    for tab_type in tab_types:
+        table_dict[tab_type] = {}
+        meta_dict[tab_type] = {}
+
+    phaseshort_tab_names = glob(f"{inp_path}/*phaseshortgaincal_time_phase*.txt")
+
+    # Sort by SPW and create a text
+    ant_nums = [int(tab.rstrip(".txt").split("ant")[1]) for tab in phaseshort_tab_names]
+
+    for ant in ant_nums:
+
+        # There aren't many to loop through.
+        for ant_name in phaseshort_tab_names:
+
+            if f"_ant{ant}.txt" in ant_name:
+                break
+
+        phase_out = read_casa_txt(ant_name)
+
+        table_dict['phase'][ant] = phase_out[0]
+
+        meta_dict['phase'][ant] = phase_out[1]
+
+    return table_dict, meta_dict
+
+
+def read_ampgaincal_time_data_tables(inp_path):
+    '''
+    Read in the BP txt files for amp and phase.
+    '''
+
+    table_dict = dict()
+    meta_dict = dict()
+
+    # Table types:
+    tab_types = ["amp"]
+
+    for tab_type in tab_types:
+        table_dict[tab_type] = {}
+        meta_dict[tab_type] = {}
+
+    ampgaincal_tab_names = glob(f"{inp_path}/*finalampgaincal_time_amp*.txt")
+
+    # Sort by SPW and create a text
+    ant_nums = [int(tab.rstrip(".txt").split("ant")[1]) for tab in ampgaincal_tab_names]
+
+    for ant in ant_nums:
+
+        # There aren't many to loop through.
+        for ant_name in ampgaincal_tab_names:
+
+            if f"_ant{ant}.txt" in ant_name:
+                break
+
+        amp_out = read_casa_txt(ant_name)
+
+        table_dict['amp'][ant] = amp_out[0]
+
+        meta_dict['amp'][ant] = amp_out[1]
+
+    return table_dict, meta_dict
+
+
+def read_ampgaincal_freq_data_tables(inp_path):
+    '''
+    Read in the BP txt files for amp and phase.
+    '''
+
+    table_dict = dict()
+    meta_dict = dict()
+
+    # Table types:
+    tab_types = ["amp"]
+
+    for tab_type in tab_types:
+        table_dict[tab_type] = {}
+        meta_dict[tab_type] = {}
+
+    ampgaincal_tab_names = glob(f"{inp_path}/*finalampgaincal_freq_amp*.txt")
+
+    # Sort by SPW and create a text
+    ant_nums = [int(tab.rstrip(".txt").split("ant")[1]) for tab in ampgaincal_tab_names]
+
+    for ant in ant_nums:
+
+        # There aren't many to loop through.
+        for ant_name in ampgaincal_tab_names:
+
+            if f"_ant{ant}.txt" in ant_name:
+                break
+
+        amp_out = read_casa_txt(ant_name)
+
+        table_dict['amp'][ant] = amp_out[0]
+
+        meta_dict['amp'][ant] = amp_out[1]
+
+    return table_dict, meta_dict
+
+
+def read_phasegaincal_data_tables(inp_path):
+    '''
+    Read in the BP txt files for amp and phase.
+    '''
+
+    table_dict = dict()
+    meta_dict = dict()
+
+    # Table types:
+    tab_types = ["phase"]
+
+    for tab_type in tab_types:
+        table_dict[tab_type] = {}
+        meta_dict[tab_type] = {}
+
+    phaseshort_tab_names = glob(f"{inp_path}/*finalphasegaincal_time_phase*.txt")
+
+    # Sort by SPW and create a text
+    ant_nums = [int(tab.rstrip(".txt").split("ant")[1]) for tab in phaseshort_tab_names]
+
+    for ant in ant_nums:
+
+        # There aren't many to loop through.
+        for ant_name in phaseshort_tab_names:
+
+            if f"_ant{ant}.txt" in ant_name:
+                break
+
+        phase_out = read_casa_txt(ant_name)
+
+        table_dict['phase'][ant] = phase_out[0]
+
+        meta_dict['phase'][ant] = phase_out[1]
 
     return table_dict, meta_dict
