546ed547f0281aaf8d02370d8aeb4cd5d6575de8
e-koch <koch.eric.w@gmail.com>
2021-09-23 12:29:50 -0400

    First pass through quicklook imaging figures with plotly; speclines working

---
+++ qaplotter/quicklook_target_imaging.py
@@ -0,0 +1,163 @@
+
+import os
+import numpy as np
+from glob import glob
+import plotly.graph_objects as go
+import plotly.express as px
+import astropy.units as u
+
+from spectral_cube import SpectralCube
+
+def make_quicklook_figures(foldername, output_foldername, suffix='image'):
+
+    if not os.path.exists(output_foldername):
+        os.mkdir(output_foldername)
+
+    data_dict = load_quicklook_images(foldername, suffix=suffix)
+
+    # Check if we have line or continuum data based on the cube shape.
+    # NOTE: this assumes we have not mixed continuum and lines.
+    targ0_dict = data_dict[list(data_dict.keys())[0]]
+    targ_spw0_dict = targ0_dict[list(targ0_dict.keys())[0]]
+
+    is_line = len(np.array(targ_spw0_dict[1].shape).squeeze()) > 2
+    type_tag = "lines" if is_line else "continuum"
+
+    for target in data_dict:
+
+        target_dict = data_dict[target]
+
+        if is_line:
+            fig = make_quicklook_lines_figure(target_dict)
+        else:
+            fig = make_quicklook_continuum_figure(target_dict)
+
+        out_html_name = f"quicklook-{target}-{type_tag}-plotly_interactive.html"
+        fig.write_html(f"{output_foldername}/{out_html_name}")
+
+
+def load_quicklook_images(foldername, suffix='image'):
+    '''
+    Split by target and SPW.
+    '''
+
+    # Gather the requested files:
+    all_cubenames = glob(f"{foldername}/*{suffix}")
+
+    # The name format is quicklook-FIELD-spwNUM-LINE/CONT-MSNAME
+    target_names = list(set([cubename.split('-')[1] for cubename in all_cubenames]))
+
+    data_dict = {}
+
+    for target in target_names:
+
+        data_dict[target] = {}
+
+        target_cubenames = [cubename for cubename in all_cubenames if target in cubename]
+
+        spw_nums = [int(cubename.split('-')[2][3:]) for cubename in target_cubenames]
+        line_names = [cubename.split('-')[3] for cubename in target_cubenames]
+
+        # SPWs aren't unique b/c we have the 1665 and 1667 lines in the same SPW.
+        # So we'll label with strings and iterate through multiples for keys.
+        for i, (spw, line, cubename) in enumerate(zip(spw_nums, line_names, target_cubenames)):
+            # NOTE: this is OK because the continuum images still have 3 dimensions.
+            # This works for now, but spectral-cube may eventually change
+            this_cube = SpectralCube.read(target_cubenames[i], format='casa')
+
+            # If an actual spectral line cube, change to VRAD
+            if this_cube.shape[0] > 1:
+                this_cube = this_cube.with_spectral_unit(u.km / u.s, velocity_convention='radio')
+
+            # Make dict label
+            current_keys = list(data_dict[target].keys())
+            i = 0
+            while True:
+                spw_label = f"{spw}_{i}"
+                if spw_label in current_keys:
+                    i += 1
+                else:
+                    break
+
+            data_dict[target][spw_label] = [line, this_cube, cubename]
+
+    return data_dict
+
+
+def make_quicklook_continuum_figure(data_dict):
+    '''
+    One figure w/ N_SPW panels for each target.
+    '''
+
+    data = np.stack([data_dict[key][1].with_fill_value(0.).unitless_filled_data[:].squeeze()
+                     for key in data_dict])
+
+
+    fig = px.imshow(data, facet_col=0, facet_col_wrap=4, facet_col_spacing=0.01,
+                    facet_row_spacing=0.025, origin='lower',
+                    color_continuous_scale='gray_r')
+
+    for i, label in enumerate(data_dict):
+        fig.layout.annotations[i]['text'] = f"SPW {label}"
+
+    return fig
+
+
+def make_quicklook_lines_figure(data_dict):
+    '''
+    One figure animated along the spectral axis w/ N_SPW panels for each target.
+
+    Note that we assume that the spectral axis is the same in all SPWs.
+    '''
+
+    # Key are in form of SPW_i, where i is the ith line in that spw.
+    spw_keys = np.array(list(data_dict.keys()))
+    line_names = np.array([data_dict[key][0] for key in data_dict])
+
+    spw_order = np.argsort([int(key.split("_")[0]) for key in spw_keys])
+
+    spw_keys_ordered = spw_keys[spw_order]
+
+    data = np.stack([data_dict[key][1].with_fill_value(0.).unitless_filled_data[:]
+                     for key in spw_keys_ordered])
+
+    # Use the HI data (if present) to estimate color range.
+    if "HI" in line_names:
+        hi_label = spw_keys[line_names == "HI"][0]
+
+        noise_rms = data_dict[hi_label][1].mad_std()
+        high_val = np.nanpercentile(data_dict[hi_label][1], 99.5)
+    else:
+        noise_rms = data_dict[list(data_dict.keys())[0]][1].mad_std()
+        high_val = np.nanpercentile(data_dict[list(data_dict.keys())[0]][1], 99.5)
+
+    noise_rms = noise_rms.value if hasattr(noise_rms, 'unit') else noise_rms
+    high_val = high_val.value if hasattr(high_val, 'unit') else high_val
+
+    low_val = -2 * noise_rms
+
+    fig = px.imshow(data, animation_frame=1, facet_col=0, binary_string=False,
+                    labels=dict(animation_frame="Channel"),
+                    origin='lower', color_continuous_scale='gray_r',
+                    range_color=[low_val, high_val])
+
+    for i, spw_label in enumerate(spw_keys_ordered):
+
+        spw = spw_label.split("_")[0]
+
+        line_label = data_dict[spw_label][0]
+        fig.layout.annotations[i]['text'] = f"SPW {spw} ({line_label})"
+
+    spec_axis = data_dict[spw_keys_ordered[0]][1].spectral_axis
+
+    # Velocity steps
+    for step in fig.layout['sliders'][0]['steps']:
+        chan_num = int(step.label)
+
+        # Update the label to include the velocity:
+        step.label = f"{chan_num} ({np.round(spec_axis[chan_num], 1)})"
+
+    fig.update_layout(autosize=True,
+                      height=600,)
+
+    return fig
