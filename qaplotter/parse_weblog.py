02b1e318ac897fb80853b4a81396270f373e268d
e-koch <koch.eric.w@gmail.com>
2021-10-05 17:00:12 -0400

    Add function to parse the flag commands and hifv_flagdata log to find issues with manual flagging

--- qaplotter/parse_weblog.py
+++ qaplotter/parse_weblog.py
@@ -5,6 +5,10 @@ Functions to extract information from the pipeline HTML weblog.
 
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
+from astropy.table import Table
+import numpy as np
+import os
 
 
 def extract_source_table(msname, weblog_name='weblog'):
@@ -46,3 +50,125 @@ def get_field_intents(fieldname, msname, weblog_name='weblog'):
         intent_string = ",".join(intent_list)
 
     return intent_string
+
+
+def extract_manual_flagging_log(msname, weblog_name='weblog'):
+    '''
+    Extract the log for the manual flagging commands to check if
+    any flags were not applied due to a typo or error.
+    '''
+
+    if 'speclines' in msname:
+        stage_num = 2
+    else:
+        # +1 for Hanning smoothing.
+        stage_num = 3
+
+    log_filename = f'{weblog_name}/html/stage{stage_num}/casapy.log'
+    flag_filename = f'{weblog_name}/html/stage{stage_num}/{msname}-agent_flagcmds.txt'
+
+    if os.path.exists(log_filename):
+        with open(log_filename) as f:
+            log_lines = f.readlines()
+    else:
+        raise ValueError(f"Unable to find log {log_filename}")
+
+    if os.path.exists(flag_filename):
+        with open(flag_filename) as f:
+            flag_lines = f.readlines()
+    else:
+        raise ValueError(f"Unable to find flag commands in {flag_filename}")
+
+    begin_task_match = "Begin Task: flagdata"
+    end_task_match = "Running the agentflagger tool"
+
+    begin_task_count = 0
+    end_task_count = 0
+
+    start_slice = None
+    end_slice = None
+
+    ii = 0
+    while True:
+
+        if begin_task_match in log_lines[ii]:
+            begin_task_count += 1
+
+            if begin_task_count == 2:
+                start_slice = ii
+
+        if end_task_match in log_lines[ii]:
+            end_task_count += 1
+
+            if end_task_count == 2:
+                end_slice = ii
+
+        ii += 1
+
+        if start_slice is not None and end_slice is not None:
+            break
+
+    if start_slice is None or end_slice is None:
+        raise ValueError("Unable to identify manual flagdata call in log.")
+
+    # Go through line-by-line where warning or errors in the log are found
+    # Line, comment, command
+    warning_command_list = []
+
+    statuses = ["WARN", 'SEVERE']
+    # for jj in range(start_slice, end_slice + 1):
+    jj = start_slice
+
+    while True:
+        # Check if WARN or ERROR in the line. If so, extract which command its coming from
+
+        if jj >= end_slice + 1:
+            break
+
+        if not any([status in log_lines[jj] for status in statuses]):
+            jj += 1
+            continue
+
+        # Skip the state expressions that are enabled by default
+        if "State Expression" in log_lines[jj]:
+            jj += 2
+            continue
+
+        # We often will flag the upper end of a SPW with +1 channels
+        # e.g. 2:60~64 for a 64 channel SPW. CASA consider the 0th channel
+        # so technically it should be 60~63. CASA fixes this but raises a harmless
+        # warning that we can skip:
+        if "parser::MSSpwIndex::convertToChannelIndex" in log_lines[jj]:
+            jj += 1
+            continue
+
+        source_string, warn_string = log_lines[jj].split("\t")[-2:]
+
+        # We expect that the only int in the message corresponds to the flag in
+        # the manual flagging file (flag_lines). e.g. Manual_2
+        int_check = re.search(r'\d+', warn_string)
+        if int_check is None:
+            flag_cmd = "Unable to identify from log."
+        else:
+            idx_flag = int(int_check.group())
+            flag_cmd = flag_lines[idx_flag]
+
+        if f"{source_string}+" in log_lines[jj+1]:
+            warn_comment = log_lines[jj+1].split("\t")[-1]
+            jj += 2
+        else:
+            warn_comment = ""
+            jj += 1
+
+        warning_command_list.append([warn_string, warn_comment, flag_cmd])
+
+    # No warnings/errors? Great!
+    if len(warning_command_list) == 0:
+        warning_command_list.append(["No issues found for manual flagging.", "Good job!", ""])
+
+    warning_command_list = np.array(warning_command_list)
+
+    # Export as an astropy table
+    tab = Table(warning_command_list, names=['Log error', 'Log comment', "Flagging command"])
+
+    return tab
