a5f78ce257c85fd79f24f280d589db2ebe452cd3
Eric Koch <koch.eric.w@gmail.com>
2025-05-20 11:23:23 -0400

    Remove deprecated astropy config

--- qaplotter/__init__.py
+++ qaplotter/__init__.py
@@ -1,10 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *   # noqa
-# ----------------------------------------------------------------------------
+from .version import version as __version__
 
 from .track_set import (make_all_plots, make_field_plots, make_all_cal_plots,
                         make_all_quicklook_plots)
